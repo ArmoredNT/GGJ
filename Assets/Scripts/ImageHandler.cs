752710cf98483ed26755c1c99f23674fd8f706ed
ArmoredNT <zachary.obrien@ontariotechu.net>
2024-01-27 13:03:07 -0500

    Added files

--- Assets/Scripts/ImageHandler.cs
+++ Assets/Scripts/ImageHandler.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+class SavedImage
+{
+    private Texture _image;
+    private Vector2 _position;
+    private float _scale;
+
+    public SavedImage(Vector2 pos, float size)
+    {
+       
+        _position = pos;
+        _scale = size;
+    }
+
+    public void SetTexture(Texture texture)
+    {
+        _image = texture;
+    }
+
+}
 
 public class ImageHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private Image sampleMask;
+    [SerializeField] private RawImage rawImage;
+    [SerializeField] private RawImage screen;
+
+    private SavedImage image;
+
+    private void Start()
     {
-        
+        image = new SavedImage(rawImage.transform.position, 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnURLEntered(string url)
     {
-        
+        StartCoroutine(Stootz(url));
+    }
+    
+    IEnumerator Stootz(string url)
+    {
+        using (var uwr = new UnityWebRequest(url, UnityWebRequest.kHttpVerbGET))
+        {
+            uwr.downloadHandler = new DownloadHandlerTexture();
+            yield return uwr.SendWebRequest();
+            //GetComponent<Renderer>().material.mainTexture
+            Texture texture = DownloadHandlerTexture.GetContent(uwr);
+            rawImage.texture = texture;
+            image.SetTexture(texture);
+            rawImage.SetNativeSize();
+            print(rawImage.uvRect);
+        }
     }
 }
---
+++ Assets/Scripts/ImageMover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
