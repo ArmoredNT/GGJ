5e1dd84c604e57dc710a3ff2c86b6b3746e4dca8
ArmoredNT <zachary.obrien@ontariotechu.net>
2024-01-26 22:51:39 -0500

    Images

--- Assets/Scripts/WebCamTester.cs
+++ Assets/Scripts/WebCamTester.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WebCamTester : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private RawImage rend;
     void Start()
     {
-        
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        // for debugging purposes, prints available devices to the console
+        for (int i = 0; i < devices.Length; i++)
+        {
+            print("Webcam available: " + devices[i].name);
+        }
+
+        //Renderer rend = this.GetComponentInChildren<Renderer>();
+
+        // assuming the first available WebCam is desired
+
+        WebCamTexture tex = new WebCamTexture(devices[0].name);
+        //rend.material.mainTexture = tex;
+        this.rend.texture = tex;
+        tex.Play();
     }
 
     // Update is called once per frame
@@ -15,4 +32,6 @@ public class WebCamTester : MonoBehaviour
     {
         
     }
+    
+    
 }
